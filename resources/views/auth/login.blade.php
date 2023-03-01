3cde7eb3ae8030ca482887e89aa5bb2f09272b16
jbflores24 <jbflores24@hotmail.com>
2023-03-01 08:51:54 -0600

    Clase 010323

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('titulo')
+    Login desde el layout
+@endsection
+@section('contenido')
+    Aquí va el contenido del login desde el layout
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('titulo')
+    Registrate en la Red Social del ITSAL
+@endsection
+@section('contenido')
+    Aquí va el contenido del Registro desde el layout
+@endsection
